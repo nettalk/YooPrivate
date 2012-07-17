4f9ab220e51238a4c877c0be453401b7333b427e
YoochulKim <nettalk@chollian.net>
2012-07-17 14:57:29 +0900

    Root Console Add

---
+++ Console/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            System.Diagnostics.Debug.WriteLine(i.ToString());
+        }
+    }
+}
