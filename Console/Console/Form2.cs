603ddad95f836237f2aad8e2577e619d5ff52d28
YoochulKim <nettalk@chollian.net>
2012-07-17 15:50:59 +0900

    Form2 추가

--- Console/Console/Form2.cs
+++ Console/Console/Form2.cs
@@ -9,16 +9,16 @@ using System.Windows.Forms;
 
 namespace Console
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(",..");
+
         }
     }
 }
